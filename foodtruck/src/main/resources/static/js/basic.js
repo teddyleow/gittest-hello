84a4d98b31b2533afb7703345071ca7133c2b6b6
teddyleow <favourite.ly@163.com>
2016-11-25 11:31:02 +0800

    fix bug

--- foodtruck/src/main/resources/static/js/basic.js
+++ foodtruck/src/main/resources/static/js/basic.js
@@ -1,4 +1,4 @@
-var init = {lat: 37.772757, lng: -122.422596},omap, marker, markList=[], Bounds, carlist;
+var init = {lat: 37.772757, lng: -122.422596},omap, marker, markList=[], Bounds, carlist=[];
 var url= '/location';
 
 function initialize(){
@@ -6,7 +6,7 @@ function initialize(){
     zoom: 16,
     center: init
   });
-  addMarker(init);
+  //addMarker(init);
   omap.addListener('zoom_changed', function(){
     Bounds = omap.getBounds().toJSON();
     getCarData(function(){
@@ -21,25 +21,32 @@ function initialize(){
   })
 
   function addMarker(location) {
+    var p = {lat: location[1], lng: location[0]};
     var marker = new google.maps.Marker({
-      position: location,
+      position:p,
       map: omap
     });
+    marker.setMap(omap);
     markList.push(marker);
   }
 
   function getCarData(cb){
     if(!!omap){
-      var data = [[Bounds.north,Bounds.east],[Bounds.south,Bounds.west]]
-      $.get(url, {bounds: Bounds}, function(res){
+      //var data = [[Bounds.south,Bounds.west],[Bounds.north,Bounds.east]]
+      //var data = [[Bounds.south,Bounds.east],[Bounds.north,Bounds.west]]
+      var data = [[Bounds.west,Bounds.north],[Bounds.east,Bounds.south]]
+      var para = "bounds=" + JSON.stringify(data);
+      $.get(url, para, function(res){
         try {
+          res = JSON.parse(res);
           if(res.length != 0){
+            carlist.length = 0;
             for(var i=0; i< res.length; i++){
               if(res[i].location.coordinates.length != 0){
-                carlist.push(res[i].location.coordinates)
+                carlist.push(res[i].location.coordinates);
               }
             }
-            cb()
+            cb();
           }
         }catch(e){
           console.log(e)
@@ -50,6 +57,9 @@ function initialize(){
   }
 
   function putTips(){
+    for(var i=0; i<markList.length; i++){
+      markList[i].setMap(null);
+    }
     markList.length = 0;
     for(var i=0; i<carlist.length; i++){
       addMarker(carlist[i])
@@ -57,6 +67,5 @@ function initialize(){
   }
 
 }
-
 
 window.onload = initialize
